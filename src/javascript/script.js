25a6e0c4a1035796aa9d0f95cccfed89966554b2
Cassiel <Cassielcdl@gmail.com>
2024-03-17 21:13:09 -0300

    lading page project

---
+++ src/javascript/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $('#mobile.btn').on('click', function() {
+    $('#mobile_menu').toggleClass('active');
+    $('#mobile_btn').find('i').toggleClass('fa-x');
+  })
+  
+  const sections = $('section');
+  const navItems = $('.nav-item');
+
+  $(window).on('scroll', function() {
+    const header = $('header');
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    let activeSectionIndex = 0;
+
+    if (scrollPosition <= 0) {
+      header.css('box-shadow', 'none');
+    } else {
+      header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+    }
+
+    sections.each(function(i){
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false;
+      }
+    })
+    navItems
+    $(navItems[activeSectionIndex]).addClass('active');
+  });
+
+  ScrollReveal().reveal('#cta', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+  });
+
+  ScrollReveal().reveal('.movie', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+  });
+
+  ScrollReveal().reveal('#release_img', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+  });
+
+  ScrollReveal().reveal('.posters', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+  });
+});
